2ec1605e52fa259a10c14dbb9a3c0edbadfe0209
filimonadrian <adrian.filimon@hotmail.com>
2020-11-12 13:05:41 +0200

    Changed declarations order

--- sol/tema1_par.h
+++ sol/tema1_par.h
@@ -8,24 +8,13 @@
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
-char *in_filename_julia;
-char *in_filename_mandelbrot;
-char *out_filename_julia;
-char *out_filename_mandelbrot;
-int P = 0;
-pthread_barrier_t barrier;
-int width, height;
-int width_mandelbrot, height_mandelbrot;
-params par;
-int **result, **result_mandelbrot;
-
-// structura pentru un numar complex
+// a complex number
 typedef struct _complex {
 	double a;
 	double b;
 } complex;
 
-// structura pentru parametrii unei rulari
+// the parameters for one run
 typedef struct _params {
 	int is_julia, iterations;
 	double x_min, x_max, y_min, y_max, resolution;
@@ -39,3 +28,14 @@ typedef struct _arguments {
 	int height;
 	int thread_id;
 } arguments;
+
+char *in_filename_julia;
+char *in_filename_mandelbrot;
+char *out_filename_julia;
+char *out_filename_mandelbrot;
+int P = 0;
+pthread_barrier_t barrier;
+int width, height;
+int width_mandelbrot, height_mandelbrot;
+params par;
+int **result, **result_mandelbrot;
